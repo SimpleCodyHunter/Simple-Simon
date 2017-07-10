d59d72d89992e150c41b7622a40a3a091c995b5d
Cody <cody@hastings.email>
2017-07-10 09:18:12 -0500

    update to structure and pseudo code

--- public/js/simplesimon.js
+++ public/js/simplesimon.js
@@ -9,6 +9,17 @@ pick a random number between 1 and 4
 3 = green
 4 = yellow
 
+		// switch (randomNum)
+		// case 1 
+		// // light up red
+		// case 2
+		// // light up blue
+		// case 3 
+		// // light up green
+		// case 4
+		// // light up yellow
+		// return pattern
+
 add random number to string. this string = computer pattern
 
 when number is added to screen display on screen color change for number by turning color on then off for 1 second then wait for user input. 
@@ -20,19 +31,30 @@ if user string does not = computer string then game over.
 otherwise game continues. computer adds another number to string and displays original pattern + 1 more number. 
 
 */
-	var red = 1
-	var blue = 2
-	var green = 3
-	var yellow = 4
-	var pattern = ""
-	var userchoice = ""
+	var red = 1;
+	var blue = 2;
+	var green = 3;
+	var yellow = 4;
+	var randomNum;
+	var pattern = [];
+	var userchoice = [];
+	var buttons = $(".buttons")
+
+
+	buttons.click(function(){
+		// light up and add number to array
+		
+	});
+
+
 
 	function addColorToPattern(){
-		
+		randomNum = Math.floor(Math.random() * 4) + 1;
+		pattern.push(randomNum)
+
 	}
 
 	function addColorToUserPattern(){
-
 	}
 
 	function comparePatterns(){
