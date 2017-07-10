be86d464890afd4540deb02f30f9cde8f7fb2a35
Cody <cody@hastings.email>
2017-07-09 23:05:21 -0500

    added pseudo code for simple simon excersise added varibles and named a few functions.

--- public/js/simplesimon.js
+++ public/js/simplesimon.js
@@ -1,11 +1,45 @@
-$(document).ready(function(){
-	
+// $(document).ready(function(){
+	"use strict"
+/* ___________PSEUDO CODE___________________
+
+pick a random number between 1 and 4 
+
+1 = red
+2 = blue
+3 = green
+4 = yellow
+
+add random number to string. this string = computer pattern
+
+when number is added to screen display on screen color change for number by turning color on then off for 1 second then wait for user input. 
+
+user clicking on color will add a number to a second string. this string = user string
+
+if user string does not = computer string then game over. 
+
+otherwise game continues. computer adds another number to string and displays original pattern + 1 more number. 
+
+*/
+	var red = 1
+	var blue = 2
+	var green = 3
+	var yellow = 4
+	var pattern = ""
+	var userchoice = ""
+
+	function addColorToPattern(){
+		
+	}
+
+	function addColorToUserPattern(){
+
+	}
+
+	function comparePatterns(){
+
+	}
 
 
 
 
-
-
-
-
-});
+// });
