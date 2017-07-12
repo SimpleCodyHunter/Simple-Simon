567ef8f484ea72d13298bb1653c6b08c600399f9
Hunter Warren <hunter.warren@gmail.com>
2017-07-12 13:34:51 -0500

    UPdated CSS! New Shoryuken sprite sheet.

--- public/js/simplesimon.js
+++ public/js/simplesimon.js
@@ -34,13 +34,9 @@
 		$(document).on("keydown", userKeyEvent);
 	})
 
-	
-
-
 
 	function startGame(){
 		gameOvervar = false
-		console.log("startGame has been called")
 		$("#start").off("click", startGame);
 		buttons.on("mousedown", addColorToUserPattern);
 		buttons.css("opacity", ".5");
@@ -51,29 +47,27 @@
 
 
 	function addColorToPattern(){
-		console.log("addColorToPattern has been called")
 		randomNum = Math.floor(Math.random() * 4) + 1;
 		pattern.push(randomNum)
-		console.log("adding " + randomNum + " to array");
-		console.log("current pattern array is " + pattern)
-		console.log("current user pattern array is " + userPattern)
+		roundNumber()
 		setTimeout(function(){
 			animatePattern()
 		}, 1000)
 	}
 
+	function roundNumber() {
+		$("#score").html("ROUND: " + pattern.length);
+		console.log(pattern.length);
+	}
 
 	function animatePattern() {
-		console.log("animatePattern has been called")
 		var count = 0;
 		var intervalId = setInterval(function () {
 			if (count >= pattern.length)  {
 				clearInterval(intervalId);
-				console.log("clearInterval(intervalID) has been called")
 			}
 			switch (pattern[count]) {
 				case 1:
-				console.log("animating red current count is " + count)
 				$("#red").css({"opacity": "1", "background-position-y": "-209px"});
 				setTimeout(function(){
 					$("#red").css({"opacity": ".5", "background-position-y": "-5px"});
@@ -81,7 +75,7 @@
 				count++;
 				break;
 				case 2:
-				console.log("animating blue current count is " + count)
+
 				$("#blue").css({"opacity": "1", "background-position-y": "-209px"});
 				setTimeout(function(){
 					$("#blue").css({"opacity": ".5", "background-position-y": "-5px"});
@@ -89,7 +83,7 @@
 				count++;
 				break;
 				case 3:
-				console.log("animating green current count is " + count)
+
 				$("#green").css({"opacity": "1", "background-position-y": "-209px"});
 				setTimeout(function(){
 					$("#green").css({"opacity": ".5", "background-position-y": "-5px"});
@@ -97,7 +91,7 @@
 				count++;
 				break;
 				case 4:
-				console.log("animating yellow current count is " + count)
+
 				$("#yellow").css({"opacity": "1", "background-position-y": "-209px"});
 				setTimeout(function(){
 					$("#yellow").css({"opacity": ".5", "background-position-y": "-5px"});
@@ -110,35 +104,30 @@
 
 
 	function addKeyToUserPattern(button) {
-		console.log("addKeyToUserPattern has been called")
 		userPattern.push(button.dataset.value);
 			$(button).css({"opacity": "1", "background-position-y": "-209px"});
-			console.log("=============this opacity is totally 1 now")
-			console.log(button);
-			console.log($(button))
 			comparePatterns()
 			animateUser(button)
 	}
+
 
 	function addColorToUserPattern() {
 		addKeyToUserPattern(this)
 		}
 
+
 	function animateUser(button) {
 			if (gameOvervar == true) {
 				$(button).css({"opacity": ".2", "background-position-y": "-5px"});
 			} else {
-				console.log(gameOvervar + " is totally false")
 				setTimeout(function(){
 				$(button).css({"opacity": ".5", "background-position-y": "-5px"});
-				console.log("changing opactiy of this to .5")
 				}, 300);
 			}
 		}
 
 		function comparePatterns(){
 		if ((pattern.length == userPattern.length) && (userPattern.toString() === pattern.toString())) {
-			console.log("you win the round");
 			userPattern = [];
 			turnCount = 0;
 			delay -= 10;
@@ -146,21 +135,19 @@
 		} else if (userPattern[turnCount] == pattern[turnCount]) {
 			turnCount += 1;
 			} else {
-				console.log("you lose")
 				gameOver();
 			}
 		}
 
+
 	function gameOver() {
 		gameOvervar = true
-		console.log("GAME OVER FUNCTION HAS BEEN CALLED")
 		buttons.off("click", addColorToUserPattern);
 		pattern = [];
 		userPattern = [];
 		delay = 450;
 		buttons.css("opacity", ".2");
 		$("#start").on("click", startGame);
-
 	}
 
 
